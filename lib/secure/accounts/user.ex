c1a5200320626efe836e9ae692cad01e00449619
Thomas Johnson <tjohnson1106@gmail.com>
2019-01-31 16:16:54 -0500

    created password hashing changeset case

--- lib/secure/accounts/user.ex
+++ lib/secure/accounts/user.ex
@@ -1,6 +1,9 @@
 defmodule Secure.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Secure.Accounts.User
+
+  import Comeonin.Bcrypt, only: [hashpwsalt: 1]
 
   schema "users" do
     field :email, :string
@@ -15,11 +18,22 @@ defmodule Secure.Accounts.User do
   @doc false
   def changeset(%User{} = user, attrs) do
     user
-    |> cast(attrs, [:email, :password_hash, :password_confirmation])
-    |> validate_required([:email, :password_hash, :password_confirmation])
-    |> validate_format([:email, ~r/@/])
-    |> validate_length([:password, min: 8])
+    |> cast(attrs, [:email, :password, :password_confirmation])
+    |> validate_required([:email, :password, :password_confirmation])
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 8)
     |> validate_confirmation(:password)
     |> unique_constraint(:email)
+    |> put_password_hash
+  end
+
+  defp put_password_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, hashpwsalt(pass))
+
+      _ ->
+        changeset
+    end
   end
 end
