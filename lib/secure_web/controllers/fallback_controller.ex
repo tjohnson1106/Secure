da8d0565e54d7a6c1a419a3f005c7be1537f71bd
Thomas Johnson <tjohnson1106@gmail.com>
2019-02-04 17:45:26 -0500

    created call function to handle unauthorized errors

--- lib/secure_web/controllers/fallback_controller.ex
+++ lib/secure_web/controllers/fallback_controller.ex
@@ -19,4 +19,10 @@ defmodule SecureWeb.FallbackController do
     |> put_view(SecureWeb.ErrorView)
     |> render(:"404")
   end
+
+  def call(conn, {:error, :unauthorized}) do
+    conn
+    |> put_status(:unauthorized)
+    |> json(%{error: "Login error"})
+  end
 end
